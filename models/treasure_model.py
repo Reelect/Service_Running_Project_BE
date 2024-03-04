58cdb80b5189d37012820494e97519fd12191108
Jaesun Lee <rejaealsun@gist.ac.kr>
2024-03-04 21:17:13 +0900

    Update treasure_model.py

--- models/treasure_model.py
+++ models/treasure_model.py
@@ -8,7 +8,7 @@ from core.db import Base
 
 class Ranger(Base):
     __tablename__ = "ranger"
-    ranger_id = Column(String, primary_key=True, default=lambda: str(uuid.uuid4()), unique=True, nullable=False)
+    ranger_id = Column(String(36), primary_key=True, default=lambda: str(uuid.uuid4()), unique=True, nullable=False)
     nickname = Column(VARCHAR(30), nullable=False, unique=True)
     ph_number = Column(VARCHAR(20), nullable=False, unique=True)
     treasure1 = Column(TINYINT, default=0)
